e015ae5ca0289844f735d150ed9fe97ed5ab841a
kakaa2993@ <kakaa2993@gmail.com>
2022-07-25 13:41:56 -0700

    complete all the requirenments

--- main.py
+++ main.py
@@ -5,21 +5,38 @@ import random
 import os
 from art import logo
 
-numbers = [number for number in range(1, 101)]
 
 # print the first display:
 os.system('clear')
-print(log)
+print(logo)
 
 
-def the_right_number():
-    global numbers
-    answer = random.choice(numbers)
-    return answer
+answer = random.randint(1,100)
 
-def display_message():
-    print("""Welcome Welcome to the Number Guessing Game!
-I'm thinking of a number between 1 and 100.""")
+print("Welcome Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.")
+print(answer)
+num_of_attempts = 10
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+if difficulty == "hard":
+    num_of_attempts = 5
+
+def guessing_the_number(attempts):
+    global answer
+    while attempts > 0:
+        print(f'You have {attempts} attempts remaining to guess the number. ')
+        guess = int(input("Make a guess: "))
+        if guess == answer:
+            print(f"You got it! The answer was {answer}.😃 ")
+            attempts = 0
+        elif guess > answer:
+            print("Too high.\nGuess agian.")
+        elif guess < answer:
+            print("Too low.\nGuess agian.")
+        attempts -= 1
+    if attempts == 0:
+        print("You've run out of guesses, you lose.🤪")
+
+guessing_the_number(attempts=num_of_attempts)
 
 
-difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+
