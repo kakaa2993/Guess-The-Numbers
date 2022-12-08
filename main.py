c63982fd2f15c1535d00f336e2f62aa47e0541f7
kakaa2993@ <kakaa2993@gmail.com>
2022-12-08 22:31:59 +0100

    add the nececary important modules

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 #!/usr/bin/python3
+from flask import Flask, render_template, redirect, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+app.secret_key = ""
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_TRACK_NOTIFICATION"] = False
+Bootstrap(app=app)
+db = SQLAlchemy(app=app)
+
+class Movies(db.Model):
+    pass
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="localhost", port=5000)
 
